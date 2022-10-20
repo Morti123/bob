a000f2981dda3b55dfdef3fb3f3ca32ec4f7c27c
Morti123 <114169876+Morti123@users.noreply.github.com>
2022-10-20 18:30:32 +0300

    Add files via upload

---
+++ Bogdan20.10.2022.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	//n18
+	/*int n;
+	cin >> n;
+	int i = 1;
+	int k = 0;
+	while (i < n) {
+		if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+			k += 1;
+		}
+		i += 1;
+	}
+	cout << k << endl;*/
+	//1
+	/*int i = 1;
+	while (i < 99) {
+		if (i % 7 == 0) {
+			cout << i << " ";
+		}
+		i += 1;
+	}*/
+	//n2
+	/*int n;
+	cin >> n;
+	int i = 1;
+	while (i<=n) {
+		if (i <= n) {
+			cout << "&";
+		}
+		i += 1;
+	}*/
+	//n3
+	/*int m = 0;
+	int i = 20;
+	while (i <= 78) {
+		if (i % 2 == 0) {
+			m = m + i;
+		}
+		i += 1;
+	}cout << m;*/
+	//n4
+	/*int a, b;
+	int k=1;
+	cin >> a >> b;
+	int i = a;
+	while (i <= b) {
+		if (i % 9 == 2 || i % 9== 3) {
+			k = k * i;
+		}
+		i+=1;
+	}
+	cout <<k<< endl;*/
+	//n5
+	/*int a, b;
+	cin >> a >> b;
+	int i = b;
+	while (i > a) {
+		if (i % 7 == 0) {
+			cout << i << " ";
+		}
+		i -= 1;
+	}
+	cout << i << " " << endl;*/
+
+
+
+
+
+
+	return 0;
+}
+
